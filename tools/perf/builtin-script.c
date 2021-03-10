8c3e6c8aff99a6d341eeea41abe67eab92f6a417
Daniel Shaulov <daniel.shaulov@granulate.io>
2021-03-10 11:46:12 +0200

    perf: Use exec_comm instead of comm when printing
    Process names are much clearer as the first line in a flamegraph
    If we ever want to upstream this, it needs to be a separate option

--- tools/perf/builtin-script.c
+++ tools/perf/builtin-script.c
@@ -635,11 +635,11 @@ static int perf_sample__fprintf_start(struct perf_sample *sample,
 
 	if (PRINT_FIELD(COMM)) {
 		if (latency_format)
-			printed += fprintf(fp, "%8.8s ", thread__comm_str(thread));
+			printed += fprintf(fp, "%8.8s ", thread__exec_comm_str(thread));
 		else if (PRINT_FIELD(IP) && evsel__has_callchain(evsel) && symbol_conf.use_callchain)
-			printed += fprintf(fp, "%s ", thread__comm_str(thread));
+			printed += fprintf(fp, "%s ", thread__exec_comm_str(thread));
 		else
-			printed += fprintf(fp, "%16s ", thread__comm_str(thread));
+			printed += fprintf(fp, "%16s ", thread__exec_comm_str(thread));
 	}
 
 	if (PRINT_FIELD(PID) && PRINT_FIELD(TID))
--- tools/perf/util/thread.h
+++ tools/perf/util/thread.h
@@ -90,6 +90,7 @@ int thread__comm_len(struct thread *thread);
 struct comm *thread__comm(const struct thread *thread);
 struct comm *thread__exec_comm(const struct thread *thread);
 const char *thread__comm_str(struct thread *thread);
+const char *thread__exec_comm_str(struct thread *thread);
 int thread__insert_map(struct thread *thread, struct map *map);
 int thread__fork(struct thread *thread, struct thread *parent, u64 timestamp, bool do_maps_clone);
 size_t thread__fprintf(struct thread *thread, FILE *fp);
