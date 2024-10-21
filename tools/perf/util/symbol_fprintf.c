ecde4eb1f9ab846c419b9d7b3191621448bdd63a
slicklash <slicklash@gmail.com>
2024-10-21 16:46:57 +0200

    add perf -F +symline

--- tools/perf/util/symbol_fprintf.c
+++ tools/perf/util/symbol_fprintf.c
@@ -6,6 +6,7 @@
 #include "dso.h"
 #include "map.h"
 #include "symbol.h"
+#include "srcline.h"
 
 size_t symbol__fprintf(struct symbol *sym, FILE *fp)
 {
@@ -16,28 +17,53 @@ size_t symbol__fprintf(struct symbol *sym, FILE *fp)
 		       sym->name);
 }
 
-size_t __symbol__fprintf_symname_offs(const struct symbol *sym,
+size_t __symbol__fprintf_sym_offs(const struct symbol *sym,
 				      const struct addr_location *al,
 				      bool unknown_as_addr,
-				      bool print_offsets, FILE *fp)
+				      bool print_offsets,
+				      bool print_line,
+                      FILE *fp)
 {
 	unsigned long offset;
 	size_t length;
 
 	if (sym) {
 		length = fprintf(fp, "%s", sym->name);
-		if (al && print_offsets) {
+		if (al && (print_offsets || print_line)) {
 			if (al->addr < sym->end)
 				offset = al->addr - sym->start;
 			else
 				offset = al->addr - map__start(al->map) - sym->start;
-			length += fprintf(fp, "+0x%lx", offset);
+            if (print_line) {
+                int ret = 0;
+                char *srcline = map__srcline(al->map, al->addr, NULL);
+                if (srcline != SRCLINE_UNKNOWN) {
+                    ret = fprintf(fp, "+%s", srcline);
+                    if (ret > 0) length += (size_t)ret;
+                }
+                srcline = map__srcline(al->map, sym->start, NULL);
+                if (srcline != SRCLINE_UNKNOWN) {
+                    ret = fprintf(fp, "+%s", srcline);
+                    if (ret > 0) length += (size_t)ret;
+                }
+                zfree_srcline(&srcline);
+			}
+            else
+                length += fprintf(fp, "+0x%lx", offset);
 		}
 		return length;
 	} else if (al && unknown_as_addr)
 		return fprintf(fp, "[%#" PRIx64 "]", al->addr);
 	else
 		return fprintf(fp, "[unknown]");
+}
+
+size_t __symbol__fprintf_symname_offs(const struct symbol *sym,
+				      const struct addr_location *al,
+				      bool unknown_as_addr,
+				      bool print_offsets, FILE *fp)
+{
+    return __symbol__fprintf_sym_offs(sym, al, unknown_as_addr, print_offsets, false, fp);
 }
 
 size_t symbol__fprintf_symname_offs(const struct symbol *sym,
@@ -71,3 +97,10 @@ size_t dso__fprintf_symbols_by_name(struct dso *dso,
 	}
 	return ret;
 }
+
+size_t symbol__fprintf_symline_offs(const struct symbol *sym,
+				      const struct addr_location *al, FILE *fp)
+{
+	return __symbol__fprintf_sym_offs(sym, al, false, false, true, fp);
+}
+
